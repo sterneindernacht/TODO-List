ea4abcdf0872cfe11cd44771a9f30499056e697d
sterneindernacht <kaczmarek.anita0@gmail.com>
2020-02-16 22:58:45 +0100

    changed JS File

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
-1
+let todoList = null;
+let todoForm = null;
+let todoSearch = null;
+
+function addTask(text) {
+    console.log("I'm adding new task to the list")
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    const todoList = document.querySelector("#todoList");
+    const todoForm = document.querySelector("#todoForm");
+    const todoSearch = document.querySelector("#todoSearch");
+
+
+    todoForm.addEventListener("submit", function(e) {
+        e.preventDefault();
+        const taskInput = this.querySelector("#taskInput");
+        if (taskInput.value !== "") {
+            addTask(taskInput.value);
+            taskInput.value = "";
+        }
+
+
+    });
+
+});
