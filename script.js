d16f39678343ab99f601bd7ef66a5d5286016ffb
sterneindernacht <kaczmarek.anita0@gmail.com>
2020-02-17 16:54:25 +0100

    DOM

--- script.js
+++ script.js
@@ -6,13 +6,13 @@ function addTask(text) {
     console.log("I'm adding new task to the list")
 }
 
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
     const todoList = document.querySelector("#todoList");
     const todoForm = document.querySelector("#todoForm");
     const todoSearch = document.querySelector("#todoSearch");
 
 
-    todoForm.addEventListener("submit", function(e) {
+    todoForm.addEventListener("submit", function (e) {
         e.preventDefault();
         const taskInput = this.querySelector("#taskInput");
         if (taskInput.value !== "") {
