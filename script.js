5c0ae5599dc766a0437695f0bafed337b54ee4fd
sterneindernacht <kaczmarek.anita0@gmail.com>
2020-02-18 22:13:09 +0100

    Attributes added to elements

--- script.js
+++ script.js
@@ -10,14 +10,18 @@ function addTask(text) {
     const divDate = document.createElement("div");
 
     const hDate = document.createElement("h6");
+    hDate.setAttribute("id", "date");
 
     const divTask = document.createElement("div");
 
     const task = document.createElement("div");
     task.classList.add("all-tasks");
+    task.setAttribute("id", "tasks-all");
 
     const deleteButton = document.createElement("button");
     deleteButton.classList.add("all-tasks");
+    deleteButton.setAttribute("id", "button-delete");
+    deleteButton.title = "delete task";
 
     const iconDelete = document.createElement("i");
     iconDelete.classList.add("fas fa-trash-alt");
