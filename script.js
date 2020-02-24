47778104553e1a941c4d4f30d3f3fa8dc052314f
sterneindernacht <kaczmarek.anita0@gmail.com>
2020-02-24 21:08:01 +0100

    finished done tasks

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let todoList = null;
 let todoForm = null;
 let todoSearch = null;
+let doneList = null;
 
 function addTask(text) {
 
@@ -57,7 +58,55 @@ function addTask(text) {
 
 //function to clone added task to done tasks
 
-function doneTask() {
+function addDoneTask(text) {
+    const doneTasks = document.createElement("div");
+    doneTasks.classList.add("done-tasks");
+
+    const divDate = document.createElement("div");
+
+    const hDate = document.createElement("h6");
+    hDate.setAttribute("id", "date");
+    const date = new Date();
+    const dateText = date.getDate() + "-" + date.getMonth() + "-" + date.getFullYear() + " " + date.getHours() + ":" + date.getMinutes();
+    hDate.innerHTML = dateText;
+
+    const divTask = document.createElement("div");
+
+    const checkDiv = document.createElement("div");
+    checkDiv.classList.add("all-tasks");
+
+
+    const checkTask = document.createElement("input");
+    checkTask.classList.add("all-tasks");
+    checkTask.setAttribute("type", "checkbox");
+    checkTask.setAttribute("id", "done-checkbox");
+
+
+    const task = document.createElement("div");
+    task.classList.add("all-tasks");
+    task.setAttribute("id", "tasks-all");
+    task.innerHTML = text;
+
+
+    const deleteButton = document.createElement("button");
+    deleteButton.classList.add("all-tasks");
+    deleteButton.setAttribute("id", "button-delete");
+    deleteButton.title = "delete task";
+
+    const iconDelete = document.createElement("i");
+    iconDelete.classList.add("fas", "fa-trash-alt");
+
+    deleteButton.appendChild(iconDelete);
+    divDate.appendChild(hDate);
+    checkDiv.appendChild(checkTask);
+    divTask.appendChild(checkTask);
+    divTask.appendChild(task);
+
+    divTask.appendChild(deleteButton);
+
+    doneTasks.appendChild(divDate);
+    doneTasks.appendChild(divTask);
+    doneList.appendChild(doneTasks);
 
 
 }
@@ -66,6 +115,7 @@ document.addEventListener("DOMContentLoaded", function () {
     todoList = document.querySelector("#todoList");
     const todoForm = document.querySelector("#todoForm");
     const todoSearch = document.querySelector("#todoSearch");
+    doneList = document.querySelector(".done-tasks");
 
 
     todoForm.addEventListener("submit", function (e) {
@@ -79,6 +129,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
     });
 
+
     todoList.addEventListener("click", function (e) {
         if (e.target.closest("#button-delete") !== null) {
             e.target.closest(".todo-element-bar").remove();
@@ -90,6 +141,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
     todoList.addEventListener("click", function (e) {
         if (e.target.closest("#done-checkbox") !== null) {
+            addDoneTask(e.target.parentNode.querySelector("#tasks-all").innerText);
             e.target.closest(".todo-element-bar").remove();
         }
 
